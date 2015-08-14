0532da5fb929e5ef645e6a95cfa0c93c5c28b36e
Ammar Husain <ammo700@gmail.com>
2015-08-14 02:27:52 -0700

    Added documentation

--- Workspaces/LessonA1/src/DrawHouse.java
+++ Workspaces/LessonA1/src/DrawHouse.java
@@ -1,15 +1,31 @@
 import gpdraw.*;
-
+/**
+ * This class creates a canvas and a marker that can be used
+ * to draw a house. You have the ability to choose
+ * if you want doors and windows or not.
+ * @author Ammar Husain
+ *
+ */
 public class DrawHouse {
 	private DrawingTool marker;
 	private SketchPad poster;
-
+	/**
+	 * The constructor creates a SketchPad with a set length and
+	 * a marker that is linked to the SketchPad
+	 */
 	public DrawHouse(){
 		poster = new SketchPad(500, 500);
 		marker = new DrawingTool(poster);
 	}
-	
+	/**
+	 * Draws the house based on user selected features. Can choose 
+	 * to draw or not to draw houses and a door.
+	 * @param door if user wants a door
+	 * @param windows if user wants windows
+	 * @see House with user specified features
+	 */
 	public void draw(boolean door, boolean windows){
+		//Draws General House First
 		marker.up();
 		marker.move(200,-200);
 		marker.down();
@@ -23,6 +39,7 @@ public class DrawHouse {
 		marker.move(0,200);
 		marker.move(-200,100);
 		marker.up();
+		//Checks if user wants door
 		if (door) {
 			marker.move(-25,-200);
 			marker.down();
@@ -31,6 +48,7 @@ public class DrawHouse {
 			marker.move(25,-200);
 			marker.up();
 		}
+		//Checks if user wants windows
 		if (windows) {
 			marker.move(-75,-25);
 			marker.down();
--- Workspaces/LessonA1/src/drawhousedriver.java
+++ Workspaces/LessonA1/src/drawhousedriver.java
@@ -1,11 +1,10 @@
 
 public class drawhousedriver {
-
 	
 	public static void main(String[] args) 
 	{
 		DrawHouse myHouse = new DrawHouse();
-		myHouse.draw(true,true);
+		myHouse.draw(true,true); //Pass parameters for doors and windows
 
 	}// end method main
 
