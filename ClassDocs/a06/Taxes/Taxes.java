adae69cb0d56cc975a64cbb17f97fa16b7d1326b
Ammar Husain <ammo700@gmail.com>
2015-09-21 17:15:21 +0000

    Expanded getInfo method, added calculation for first three lines

--- ClassDocs/a06/Taxes/Taxes.java
+++ ClassDocs/a06/Taxes/Taxes.java
@@ -1,23 +1,26 @@
 public class Taxes{
-    private final double FICA = 6.7;
-    private final double STATE_TAX = 6.7;
+    private final double FICA = 7.65;
+    private final double STATE_TAX = 4.00;
+    private final double FEDERAL_TAX = 15.00;
     private int hoursWorked;
     private double hourlyRate;
 
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        Taxes ammu = new Taxes(5, 15.5);
+        ammu.getInfo();
     }
-    public Taxes(){
-        
+    public Taxes(int hours, double rate){
+        hoursWorked = hours;
+        hourlyRate = rate;
     }
+    
     public void getInfo(){
-        System.out.printf("%25s%8.2f%n","", );
-        System.out.printf("%25s%8.2f%n","", );
-        System.out.printf("%25s%8.2f%n","", );
-        System.out.printf("%25s%8.2f%n","", );
-        System.out.printf("%25s%8.2f%n","", );
-        System.out.printf("%25s%8.2f%n","", );
-        System.out.printf("%25s%8.2f%n","", );
-        System.out.printf("%25s%8.2f%n","", );
+        System.out.printf("%25s%8.2f%n","Hours Worked:", hoursWorked);
+        System.out.printf("%25s%8.2f%n","Hourly rate:", hourlyRate);
+        System.out.printf("%25s%8.2f%n","Gross pay:", hoursWorked*hourlyRate);
+        System.out.printf("%25s%8.2f%n","Federal tax (15.00%):", );
+        System.out.printf("%25s%8.2f%n","FICA (7.65%):", );
+        System.out.printf("%25s%8.2f%n","State tax (4.00%):", );
+        System.out.printf("%25s%8.2f%n","Net pay:", );
     }
 }
