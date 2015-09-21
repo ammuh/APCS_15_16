725b615db2dde27b72bc77bd8073bf94b1bcc9b1
Ammar Husain <ammo700@gmail.com>
2015-09-21 17:04:36 +0000

    Created instance variables for taxes and shell for getInfo method

--- ClassDocs/a06/Taxes/Taxes.java
+++ ClassDocs/a06/Taxes/Taxes.java
@@ -1,5 +1,23 @@
 public class Taxes{
+    private final double FICA = 6.7;
+    private final double STATE_TAX = 6.7;
+    private int hoursWorked;
+    private double hourlyRate;
+
     public static void main(String args[]){
         System.out.println("Hello World!");
+    }
+    public Taxes(){
+        
+    }
+    public void getInfo(){
+        System.out.printf("%25s%8.2f%n","", );
+        System.out.printf("%25s%8.2f%n","", );
+        System.out.printf("%25s%8.2f%n","", );
+        System.out.printf("%25s%8.2f%n","", );
+        System.out.printf("%25s%8.2f%n","", );
+        System.out.printf("%25s%8.2f%n","", );
+        System.out.printf("%25s%8.2f%n","", );
+        System.out.printf("%25s%8.2f%n","", );
     }
 }
