134c452482792971b968870149853ca67c41b6e6
Ammar Husain <ammo700@gmail.com>
2015-09-23 17:20:45 +0000

    Added shell for polygon class

---
+++ ClassDocs/a06/Polygon/RegularPolygon.java
@@ -0,0 +1,41 @@
+public class RegularPolygon{ 
+private int myNumSides; // t of sides 
+private double mySideLength; // length of side 
+private double myR; // radius of circumscribed circle 
+private double myr; // radius of inscribed circle 
+// constructors 
+public RegularPolygon(){
+    
+} 
+public RegularPolygon(int numSides, double sideLength){
+    
+} 
+// private methods 
+private void calcr(){
+    
+}
+private void calcR(){
+    
+} 
+// public methods 
+public double vertexAngle(){
+    
+} 
+public double Perimeter(){
+    
+} 
+public double Area(){
+    
+} 
+public double getNumside(){
+    
+} 
+public double getSideLength(){
+    
+} 
+public double getR(){
+    
+} 
+public double getr(){
+    
+} 
