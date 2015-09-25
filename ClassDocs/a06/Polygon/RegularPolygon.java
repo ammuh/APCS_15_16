ce97553221def0929655f024bf9585c1422d14d2
Ammar Husain <ammo700@gmail.com>
2015-09-25 10:43:45 -0700

    Created the final versions of the classes, uploaded to gdrive

--- ClassDocs/a06/Polygon/RegularPolygon.java
+++ ClassDocs/a06/Polygon/RegularPolygon.java
@@ -1,57 +1,89 @@
-public class RegularPolygon{ 
-private int myNumSides; // t of sides 
-private double mySideLength; // length of side 
-private double myR; // radius of circumscribed circle 
-private double myr; // radius of inscribed circle 
-// constructors 
-public RegularPolygon(){
-    myNumSides = 3;
-} 
-public RegularPolygon(int numSides, double sideLength){
-    myNumSides = numSides;
-    mySideLength = sideLength;
-    this.calcR();
-    this.calcr();
-    
-} 
-// private methods 
-private void calcr(){
-    this.myr = .5*mySideLength*(1.0/Math.tan(Math.PI/(double)myNumSides));
+public class RegularPolygon {
+	private int myNumSides; // t of sides 
+	private double mySideLength; // length of side 
+	private double myR; // radius of circumscribed circle 
+	private double myr; // radius of inscribed circle 
+	/**
+	 * constructor RegularPolygon - initializes the polygon to a triangle
+	 */
+	public RegularPolygon() {
+		myNumSides = 3;
+	}
+	/**
+	 * constructor RegularPolygon - initializes the polygon specified side lengths and number of sides
+	 * @param numSides number of sides
+	 * @param sideLength Length of the sides on the polygon
+	 */
+	public RegularPolygon(int numSides, double sideLength) {
+		myNumSides = numSides;
+		mySideLength = sideLength;
+		this.calcR();
+		this.calcr();
 
-}
-private void calcR(){
-    this.myR = .5*mySideLength*(1.0/Math.sin(Math.PI/(double)myNumSides));
+	}
+	// private methods 
+	/**
+	 * Calculates the radius of an inscribed circle.
+	 */
+	private void calcr() {
+		this.myr = .5 * mySideLength * (1.0 / Math.tan(Math.PI / (double) myNumSides));
 
-} 
-// public methods 
-public double vertexAngle(){
-    double vangle = ((myNumSides-2)/myNumSides)*180;
- 
-    return vangle;
-} 
-public double Perimeter(){
-    double perimeter = myNumSides*mySideLength;
-  
-    return perimeter;
-} 
-public double Area(){
-    double area = .5*myNumSides*Math.pow(myR, 2)*Math.sin(2*Math.PI/myNumSides);
-   
-    return area;
-} 
-public int getNumside(){
+	}
+	/**
+	 * Calculates the radius of a circumscribed circle.
+	 */
+	private void calcR() {
+		this.myR = .5 * mySideLength * (1.0 / Math.sin(Math.PI / (double) myNumSides));
 
-    return myNumSides;
-} 
-public double getSideLength(){
-   
-    return mySideLength;
-} 
-public double getR(){
-  
-    return myR;
-} 
-public double getr(){
-     return myr;
-} 
+	}
+	// public methods 
+	/**
+	 * Calculates the angle of any vertex in the polygon
+	 * @return The angle of the vertex
+	 */
+	public double vertexAngle() {
+		return ((myNumSides - 2.0) / myNumSides) * 180.0;
+	}
+	/**
+	 * Calculates the perimeter of the shape
+	 * @return perimeter of polygon
+	 */
+	public double Perimeter() {
+		return myNumSides * mySideLength;
+	}
+	/**
+	 * Calculates the area of the polygon.
+	 * @return area
+	 */
+	public double Area() {
+		return .5 * myNumSides * Math.pow(myR, 2) * Math.sin(2 * Math.PI / myNumSides);
+	}
+	/**
+	 * Accessor methods that gets the number of sides
+	 * @return number of sides
+	 */
+	public int getNumside() {
+		return myNumSides;
+	}
+	/**
+	 * Accessor method that gets the length of the side
+	 * @return Side length
+	 */
+	public double getSideLength() {
+		return mySideLength;
+	}
+	/**
+	 * Accessor method that gets the radius of the circumscribed circle.
+	 * @return radius of the circle
+	 */
+	public double getR() {
+		return myR;
+	}
+	/**
+	 * Accessor method that gets the radius of the inscribed circle.
+	 * @return radius of the circle
+	 */
+	public double getr() {
+		return myr;
+	}
 }
--- ClassDocs/a06/Taxes/Taxes.java
+++ ClassDocs/a06/Taxes/Taxes.java
@@ -11,54 +11,87 @@ public class Taxes{
     private final double FEDERAL_TAX = .15;
     private double hoursWorked;
     private double hourlyRate;
-    private double grossPay;
-    private double socTax;
-    private double stateTax;
-    private double fedTax;
-    private double totalTax;
-    private double netPay;
-
+	/**
+	 * constructor Taxes - initializes the hoursWorked and hourlyRate
+	 */
     public Taxes(double hours, double rate){
         hoursWorked = hours;
         hourlyRate = rate;
     }
+	/**
+	 * Accessor method to get hours worked
+	 * @return the hours worked
+	 */
     public double getHoursWorked(){
         return hoursWorked;
     }
+	/**
+	 * Accessor method to get hourly rate
+	 * @return the hourly rate
+	 */	
     public double getHourlyRate(){
         return hourlyRate;
     }
+	/**
+	 * Accessor method to get federal tax rate
+	 * @return the federal tax rate
+	 */
     public double getFedTaxRate(){
         return FEDERAL_TAX;
     }
+    /**
+	 * Accessor method to get social security tax rate
+	 * @return the social security tax rate
+	 */	
     public double getSocSecurityRate(){
         return FICA;
     }
+    /**
+	 * Accessor method to get state tax rate
+	 * @return the state tax rate
+	 */
     public double getStateTaxRate(){
         return STATE_TAX;
     }
+    /** Method to compute the gross pay - pay before taxes
+	 * @return gross pay
+	 */
      public double computeGrossPay(){
-        grossPay = hourlyRate*hoursWorked;
-        return grossPay;
+        return hourlyRate*hoursWorked;
     }
+     /**
+ 	 * Method to compute the amount of federal tax
+ 	 * @return federal tax
+ 	 */
      public double computeFedTax(){
-        fedTax= grossPay*FEDERAL_TAX;
-        return fedTax;
+        return computeGrossPay()*FEDERAL_TAX;
     }
+     /**
+ 	 * Method to compute the amount of social security tax
+ 	 * @return social security tax
+ 	 */
     public double computeSocSecurity(){
-        socTax = FICA*grossPay;
-        return socTax;
+        return FICA*computeGrossPay();
     }
+    /**
+	 * Method to compute the amount of state tax
+	 * @return state tax
+	 */
     public double computeStateTax(){
-        stateTax = STATE_TAX*grossPay;
-        return stateTax;
+        return STATE_TAX*computeGrossPay();
     }
+    /**
+	 * Method to compute the amount of total tax
+	 * @return total tax
+	 */
     public double computeTotalTax(){
-        totalTax = stateTax + socTax + fedTax;
-        return totalTax;
+        return computeStateTax() + computeFedTax() + computeSocSecurity();
     }
+    /**
+	 * Method to compute the amount of net pay - pay after taxes
+	 * @return net pay
+	 */
     public double computeNetPay(){
-        netPay = grossPay - computeTotalTax();
-        return netPay;
+        return computeGrossPay() - computeTotalTax();
     }
 }
