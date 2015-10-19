c84b715ad97399c1fa3bcb225f821236d3bfa41e
unknown <husainammar8236@EV-B138-11.esuhsd.org>
2015-10-19 11:11:18 -0700

    Added documentation for Koch Curve class

--- ClassDocs/A09/src/KochCurve.java
+++ ClassDocs/A09/src/KochCurve.java
@@ -7,10 +7,22 @@ import gpdraw.*;
 public class KochCurve {
     public SketchPadWithNoDelay canvas;
     public DrawingTool pen;
-     public KochCurve(int l, int w, int posx, int posy){
+    /**
+     * Constructor that creates a sketchpad and pen for the specific curve to be drawn.
+     * @param l Length of the window.
+     * @param w Width of the window. 
+     * @param posx X position of the koch curve starting position. 
+     * @param posy Y position of the koch curve starting position.
+     */
+    public KochCurve(int l, int w, int posx, int posy){
         canvas = new SketchPadWithNoDelay(l, w, posx, posy);
         pen = new DrawingTool(canvas);
     }
+    /**
+     * Draws a snowflake consisting of three koch curves positioned in a triangle shape.
+     * @param level Level of iterations for the Koch Curves.
+     * @param length Length of the base iteration.
+     */
     public void drawSnowflake(int level, double length){
         this.drawKochCurve(level, length);
         this.pen.turnRight(120);
@@ -18,6 +30,11 @@ public class KochCurve {
         this.pen.turnRight(120);
         this.drawKochCurve(level, length);
     }
+    /**
+     * Draws a Koch curve recursively by drawing smaller iterations of the spiked edge.
+     * @param level Level of iterations for the Koch Curves.
+     * @param length Length of the base iteration.
+     */
     public void drawKochCurve(int level, double length){
         if(level < 1){
             pen.down();
