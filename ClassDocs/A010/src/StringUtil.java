d8c16d0e49bb238715645e6a1f1cc79e8c5da2b9
Ammar Husain <ammo700@gmail.com>
2015-10-23 20:54:22 -0700

    Finished String Util, all methods work

--- ClassDocs/A010/src/StringUtil.java
+++ ClassDocs/A010/src/StringUtil.java
@@ -1,42 +1,80 @@
-public class StringUtil{
-    public static void main(String args[]){
-        System.out.print(reverse("Car"));
+public class StringUtil {
+    public static void main(String args[]) {
+        System.out.print(shorthand(""));
     }
-    public static String reverse(String s){
-        if(!s.isEmpty()){
-            return "" + s.charAt(s.length()-1) + reverse(s.substring(0,s.length()-1));
-        }
-        else{
+
+    public static String reverse(String s) {
+        if (!s.isEmpty()) {
+            return "" + s.charAt(s.length() - 1) + reverse(s.substring(0, s.length() - 1));
+        } else {
             return "";
         }
     }
     public static boolean palindrome(String s){
-        return s.reverse().equals(s);
+        s = s.toLowerCase();
+        if(s.length() == 0){
+            return true;
+        }
+        if(s.length() == 1){
+            return true;
+        }
+        else {
+            char a = s.charAt(0);
+            char b = s.charAt(s.length()-1);
+            if(((int) a >= 97 && (int) a <= 122) && ((int) b >= 97 && (int) b <= 122)){
+                if(a == b) {
+                    return palindrome(s.substring(1, s.length() - 1));
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                if(!((int) a >= 97 && (int) a <= 122)){
+                    s = s.substring(1, s.length());
+                }
+                if(!((int) b >= 97 && (int) b <= 122)){
+                    if(s.length()>1){
+                        s = s.substring(0, s.length()-1);
+                    }
+                    else{
+                        s = "";
+                    }
+                }
+                return palindrome(s);
+            }
+        }
+    }
+    public static String shorthand(String s){
+        if(s.toLowerCase().indexOf("and") != -1){
+            s = s.substring(0, s.toLowerCase().indexOf("and")) + "&" + s.substring(s.toLowerCase().indexOf("and") +3, s.length());
+            return shorthand(s);
+        } else if(s.toLowerCase().indexOf("to") != -1){
+            s = s.substring(0, s.toLowerCase().indexOf("to")) + "2" + s.substring(s.toLowerCase().indexOf("to") +2, s.length());
+            return shorthand(s);
+        } else if(s.toLowerCase().indexOf("you") != -1){
+            s = s.substring(0, s.toLowerCase().indexOf("you")) + "U" + s.substring(s.toLowerCase().indexOf("you") +3, s.length());
+            return shorthand(s);
+        } else if(s.toLowerCase().indexOf("for") != -1){
+            s = s.substring(0, s.toLowerCase().indexOf("for")) + "4" + s.substring(s.toLowerCase().indexOf("for") +3, s.length());
+            return shorthand(s);
+        } else if(s.toLowerCase().indexOf('a') != -1){
+            s = s.substring(0, s.toLowerCase().indexOf('a')) + s.substring(s.toLowerCase().indexOf('a') +1, s.length());
+            return shorthand(s);
+        } else if(s.toLowerCase().indexOf('e') != -1){
+            s = s.substring(0, s.toLowerCase().indexOf('e')) + s.substring(s.toLowerCase().indexOf('e') +1, s.length());
+            return shorthand(s);
+        } else if(s.toLowerCase().indexOf('i') != -1){
+            s = s.substring(0, s.toLowerCase().indexOf('i')) + s.substring(s.toLowerCase().indexOf('i') +1, s.length());
+            return shorthand(s);
+        } else if(s.toLowerCase().indexOf('o') != -1){
+            s = s.substring(0, s.toLowerCase().indexOf('o')) + s.substring(s.toLowerCase().indexOf('o') +1, s.length());
+            return shorthand(s);
+        } else if(s.toLowerCase().indexOf('u') != -1){
+            s = s.substring(0, s.toLowerCase().indexOf('u')) + s.substring(s.toLowerCase().indexOf('u') +1, s.length());
+            return shorthand(s);
+        } else{
+            return s;
+        }
     }
 }
-/*
-. Create a recursive static method reverse that receives a String and returns a String that is
-the exact reversal of the characters in the first String. The output should be in the following
-format:
-Reverse of <original string> is <reversed string>.
-Any repetition must be done using recursion (do not use loops). Use only the methods
-taught.
-2. Create a static method palindrome that receives a String and returns a boolean value of true
-if the String is a Palindrome and false if it is not. A word is a palindrome if it reads the
-same forwards and backwards. For example, the word level is a palindrome.
-Here are two familiar examples:
-a. Madam, I'm Adam
-b. A man, a plan, a canal: Panama
-c. radar
-d. Lewd did I live, & evil I did dwel.
-Any repetition must be done using recursion (do not use loops). Use only the methods
-taught.
-3. Create a static method shorthand that receives a String and returns the String converted
-into shorthand. Do not use the replace method to do this. The simplified shorthand
-form of a string is defined as follows (Note that these actions are done in sequence):
-a. Replace these four words: “AND” with “&”, “TO” with “2”, “YOU” with “U”, and
-“FOR” with “4”. The four words can be in mixed case (upper or lower or mixed).
-b. Remove all vowels (‘a’, ‘e’, ‘i', ‘o’, ‘u’, whether lowercase or uppercase).
-Any repetition must be done using recursion (do not use loops). Use only the methods
-taught. The string may be given to you in any case (lower or upper) or mixed case.
-*/
