6be8dcc16af863bbcf124aa9ef4d2f541b3a60e4
Ammar Husain <ammo700@gmail.com>
2015-11-06 18:15:32 +0000

    String Util Modified

--- ClassDocs/A010/src/StringUtil.java
+++ ClassDocs/A010/src/StringUtil.java
@@ -4,7 +4,7 @@
  * Period 4
  */
 public class StringUtil {
-    /**
+    /** 
      * Reverses a String arguement.
      * @param s String to reverse
      * @return Reversed String
@@ -63,32 +63,23 @@ public class StringUtil {
      */
     public static String shorthand(String s){
         if(s.toLowerCase().indexOf("and") != -1){
-            s = s.substring(0, s.toLowerCase().indexOf("and")) + "&" + s.substring(s.toLowerCase().indexOf("and") +3, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf("and")) + "&" + s.substring(s.toLowerCase().indexOf("and") +3, s.length()));
         } else if(s.toLowerCase().indexOf("to") != -1){
-            s = s.substring(0, s.toLowerCase().indexOf("to")) + "2" + s.substring(s.toLowerCase().indexOf("to") +2, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf("to")) + "2" + s.substring(s.toLowerCase().indexOf("to") +2, s.length()));
         } else if(s.toLowerCase().indexOf("you") != -1){
-            s = s.substring(0, s.toLowerCase().indexOf("you")) + "U" + s.substring(s.toLowerCase().indexOf("you") +3, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf("you")) + "U" + s.substring(s.toLowerCase().indexOf("you") +3, s.length()));
         } else if(s.toLowerCase().indexOf("f" + "o" + "r") != -1){
-            s = s.substring(0, s.toLowerCase().indexOf("f" + "o" + "r")) + "4" + s.substring(s.toLowerCase().indexOf("f" + "o" + "r") +3, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf("f" + "o" + "r")) + "4" + s.substring(s.toLowerCase().indexOf("f" + "o" + "r") +3, s.length()));
         } else if(s.toLowerCase().indexOf('a') != -1){
-            s = s.substring(0, s.toLowerCase().indexOf('a')) + s.substring(s.toLowerCase().indexOf('a') +1, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf('a')) + s.substring(s.toLowerCase().indexOf('a') +1, s.length()));
         } else if(s.toLowerCase().indexOf('e') != -1){
-            s = s.substring(0, s.toLowerCase().indexOf('e')) + s.substring(s.toLowerCase().indexOf('e') +1, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf('e')) + s.substring(s.toLowerCase().indexOf('e') +1, s.length()));
         } else if(s.toLowerCase().indexOf('i') != -1){
-            s = s.substring(0, s.toLowerCase().indexOf('i')) + s.substring(s.toLowerCase().indexOf('i') +1, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf('i')) + s.substring(s.toLowerCase().indexOf('i') +1, s.length()));
         } else if(s.toLowerCase().indexOf('o') != -1){
-            s = s.substring(0, s.toLowerCase().indexOf('o')) + s.substring(s.toLowerCase().indexOf('o') +1, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf('o')) + s.substring(s.toLowerCase().indexOf('o') +1, s.length()));
         } else if(s.toLowerCase().indexOf('u') != -1){
-            s = s.substring(0, s.toLowerCase().indexOf('u')) + s.substring(s.toLowerCase().indexOf('u') +1, s.length());
-            return shorthand(s);
+            return shorthand(s.substring(0, s.toLowerCase().indexOf('u')) + s.substring(s.toLowerCase().indexOf('u') +1, s.length()));
         } else{
             return s;
         }
