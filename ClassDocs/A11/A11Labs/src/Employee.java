b054d5d59e3aabfada49e83e47cd396ccb0a1721
Ammar Husain <ammo700@gmail.com>
2015-11-14 14:56:20 -0800

    Created Employee class

---
+++ ClassDocs/A11/A11Labs/src/Employee.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ammar Husain on 11/14/2015.
+ */
+public class Employee {
+    private String name;
+    public Employee(String n){
+        name = n;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
