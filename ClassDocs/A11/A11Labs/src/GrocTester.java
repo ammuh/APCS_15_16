5de8dd058cc0b0efd59d4eed0783928adc6e00c9
Ammar Husain <ammo700@gmail.com>
2015-11-14 15:11:11 -0800

    Added all methods for groc rec, modified driver to print the groc rec, works, moving on

--- ClassDocs/A11/A11Labs/src/GrocTester.java
+++ ClassDocs/A11/A11Labs/src/GrocTester.java
@@ -3,7 +3,14 @@
  */
 public class GrocTester {
     public static void main(String args[]){
-        Item i = new Item("Condoms", .5, .2);
-        System.out.println(i);
+        Item i1 = new Item("Condoms", .5, .2);
+        Item i2 = new Item("Jb Album", 10, .2);
+        Item i3 = new Item("Buk", 7.42, .2);
+        Employee ammu = new Employee("Ammar Husain");
+        GroceryReceipt g = new GroceryReceipt(ammu);
+        g.add(i1);
+        g.add(i2);
+        g.add(i3);
+        System.out.println(g);
     }
 }
--- ClassDocs/A11/A11Labs/src/GroceryReceipt.java
+++ ClassDocs/A11/A11Labs/src/GroceryReceipt.java
@@ -11,4 +11,27 @@ public class GroceryReceipt {
         receipt = "";
         total = 0;
     }
+    public Employee getClerk(){
+        return this.clerk;
+    }
+    public String getReceipt(){
+        return this.receipt;
+    }
+    public double getTotal(){
+        return this.total;
+    }
+    public void add(Item i){
+        receipt += i.toString() + "\n";
+        total += i.getPrice();
+    }
+    public String valueToString(double d){
+        String s = "" + Math.round(d*100)/100.0;
+        if(s.length() - s.indexOf(".") <3){
+            s += "0";
+        }
+        return s;
+    }
+    public String toString(){
+        return getReceipt() + "\n" + "Total: $" + valueToString(getTotal());
+    }
 }
