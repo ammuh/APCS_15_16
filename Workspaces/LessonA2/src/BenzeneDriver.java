45e816a3d449f0a7dfb60b2134cf89672a3ce99a
Ammar Husain <ammo700@gmail.com>
2015-11-14 16:04:22 -0800

    Folder Restructure, and unessecary file delte

--- Workspaces/LessonA2/src/BenzeneDriver.java
+++
@@ -1,8 +0,0 @@
-
-public class BenzeneDriver {
-	public static void main (String[] args)
-	{
-		Benzene myBenzene = new Benzene(); //Create Benzene Object
-		myBenzene.draw(); //Draws Benzene Ring
-	}
-}
