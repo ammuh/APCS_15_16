e1fcd13566e77a38770d068d31bcbf11db3cba5e
Ammar Husain <ammo700@gmail.com>
2015-11-18 20:40:27 -0800

    GroceryReceipt lab completed, all documentations added

--- ClassDocs/A11/A11Labs/src/Employee.java
+++ ClassDocs/A11/A11Labs/src/Employee.java
@@ -1,13 +1,23 @@
 /**
- * Created by Ammar Husain on 11/14/2015.
+ * Simulates an hourly employee of a grocery store
+ * @author Ammar Husain
+ * Period 4
  */
 public class Employee {
     private String name;
 
+    /***
+     * Constructor - Creates Employee Object with a name.
+     * @param n name
+     */
     public Employee(String n){
         name = n;
     }
 
+    /***
+     * Gets the name of the Employee.
+     * @return name
+     */
     public String getName(){
         return this.name;
     }
--- ClassDocs/A11/A11Labs/src/Item.java
+++ ClassDocs/A11/A11Labs/src/Item.java
@@ -1,29 +1,49 @@
-/**
- * Created by Ammar Husain on 11/14/2015.
+/***
+ * Simulates an item in the grocery store
+ * @author Ammar Husain
+ * Period 4
  */
 public class Item {
-    private String name;
-    private double price;
-    private double discount;
-
+    private String name;//item name
+    private double price;//item unit price in dollars
+    private double discount;//item discount in dollars
+    /***
+     * Initializes the item name, price and discount
+     * @param n - item name
+     * @param p - item price in dollars
+     * @param d - discount in dollars
+     */
     public Item(String n, double p, double d){
         name = n;
         price = p;
         discount = d;
     }
-
+    /***
+     * Returns the item name
+     * @return item name
+     */
     public String getName(){
         return this.name;
     }
-
+    /***
+     * Returns the item price in dollars
+     * @return item price
+     */
     public double getPrice(){
         return this.price;
     }
-
+    /***
+     * Returns the item discount in dollars
+     * @return item discount
+     */
     public double getDiscount(){
         return this.discount;
     }
-
+    /***
+     * Returns the value passed as a string in the dollar and cents format ####.##
+     * @param d - value to be converted to dollar and cents format
+     * @return String in dollar and cents format
+     */
     public String valueToString(double d){
         String s = "" + Math.round(d*100)/100.0;
         if(s.length() - s.indexOf(".") <3){
@@ -31,6 +51,11 @@ public class Item {
         }
         return s;
     }
+    /***
+     * Returns a string with item name, price and discount in the format
+     * name item price (-discount). One item will get printed per line.
+     * For example: Spinach $3.98 (-$1.05)
+     */
     public String toString(){
         return name + " $" + valueToString(getPrice()) + " (-$" + valueToString(getDiscount()) + ")";
     }
