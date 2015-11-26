d8d764b8d542d2d1e972f1f06c403f0006dfa3f8
Ammar Husain <ammo700@gmail.com>
2015-11-25 18:38:13 -0800

    Finished GCF methods, ready for codecheck test

--- ClassDocs/A12/src/GCFAndLCM.java
+++ ClassDocs/A12/src/GCFAndLCM.java
@@ -12,8 +12,7 @@ public class GCFAndLCM {
 	 * @param b - second number
 	 * @return - the greatest common divisor
 	 */
-	public static int GCF(int a, int b) 
-	{
+	public static int GCF(int a, int b){
 		//Your code goes here
 		while(a != b){
 		    if(a > b){
@@ -33,29 +32,9 @@ public class GCFAndLCM {
 	 * @param b - second number
 	 * @return - the least common multiple
 	 */
-	public static int LCM(int a, int b)
-	{
-	    int max;
-	    int min;
-		//Your code goes here
-		if(a == b){
-		    return a;
-		}
-		if(a > b){
-		   max = a;
-		   min = b;
-		}
-		else{
-		   max = b;
-		   min = a;
-		}
-		if(max%min == 0){
-	        return max;
-	    }
-	    else if(max%min){
-	        
-	    }
-        return a;
+
+	public static int LCM(int a, int b){
+		return a * b / GCF(a,b);
 	}
 
 }
--- ClassDocs/A12/src/GCFAndLCMDriver.java
+++ ClassDocs/A12/src/GCFAndLCMDriver.java
@@ -1,10 +1,10 @@
 public class GCFAndLCMDriver {
 	public static void main(String[] args) {
-		int a =       6; 
-		int b =       12; 
+		int a = 12;
+		int b = 18;
 		System.out.println("GCF of " + a + " and " + b + " is: " + GCFAndLCM.GCF(a, b));
-		int x =       100; 
-		int y =       5; 
+		int x = 18;
+		int y = 12;
 		System.out.println("LCM of " + x + " and " + y + " is: " + GCFAndLCM.LCM(x, y));
 		
 	}	
