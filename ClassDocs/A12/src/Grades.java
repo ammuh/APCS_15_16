15aa5c1d0d4563dfdfda42389cf18a63c6a166fa
Ammar Husain <ammo700@gmail.com>
2015-11-30 20:37:41 -0800

    Grades java codecheck approved, doc complete

--- ClassDocs/A12/src/Grades.java
+++ ClassDocs/A12/src/Grades.java
@@ -2,7 +2,7 @@ import java.util.*;
 /**
  * 
  * @author Ammar Husain
- * Period #
+ * Period 4
  * 
  */
 
@@ -19,32 +19,10 @@ public class Grades {
 	public void getGradesAndCalculateGPA()
 	{
 		//Your code goes here
-		int a;
-		int b;
-		int c;
-		int d;
-		int f;
+
 		Scanner s = new Scanner(System.in);
 		System.out.print("Enter your grades: ");
 		grades =  s.nextLine();
-		for(int i = 0; i <= grades.length(); i++){
-			switch((int)grades.toLowerCase().charAt()){
-				case 97:
-				break;
-				case 98:
-				break;
-				case 99:
-				break;
-				case 100:
-				break;
-				case 102:
-				break;
-			}
-		}
-
-
-
-		
 	}
 	/**
 	 * Method to print the appropriate message
@@ -52,9 +30,44 @@ public class Grades {
 	public void printMessage()
 	{
 		//Your code goes here
-		
-
-
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int d = 0;
+		int f = 0;
+		for(int i = 0; i < grades.length(); i++){
+			switch((int)grades.toLowerCase().charAt(i)){
+				case 97:
+					a++;
+					break;
+				case 98:
+					b++;
+					break;
+				case 99:
+					c++;
+					break;
+				case 100:
+					d++;
+					break;
+				case 102:
+					f++;
+					break;
+			}
+		}
+		System.out.printf("GPA = %.2f\n", (double)(a*4+b*3+c*2+d)/(a+b+c+d+f));
+		if(a + b + c + d + f < 4){
+			System.out.println("Ineligible, taking less than 4 classes");
+		}else if(((double)(a*4+b*3+c*2+d)/(a+b+c+d+f)) < 2.0) {
+			if(f != 0){
+				System.out.println("Ineligible, gpa below 2.0 and has F grade");
+			}else{
+				System.out.println("Ineligible, gpa below 2.0");
+			}
+		}else if(f > 0){
+			System.out.println("Ineligible, gpa above 2.0 but has F grade");
+		}else{
+			System.out.println("Eligible");
+		}
 
 	}
 
